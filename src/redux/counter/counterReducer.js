91cc8e4414be22fa6502b987ec3f447a0f47ae0e
sadid369 <sadid.jones@gmail.com>
2024-12-18 05:48:09 +0600

    first commit

---
+++ src/redux/counter/counterReducer.js
@@ -0,0 +1,22 @@
+import { DECREMENT, INCREMENT } from "./actionTypes";
+
+const initialState = {
+    value: 0,
+};
+
+export const counterReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case INCREMENT:
+            return {
+                ...state,
+                value: state.value + action.payload,
+            };
+        case DECREMENT:
+            return {
+                ...state,
+                value: state.value - action.payload,
+            };
+        default:
+            return state;
+    }
+};
